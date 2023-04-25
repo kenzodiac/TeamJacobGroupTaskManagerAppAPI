d4cb7838d961772776eefbf1697b5db1de3f7ccb
Manuel <leyva4manuel@gmail.com>
2023-04-25 10:26:09 -0700

    Updated progam.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TeamJacobGroupTaskManagerAppAPI.Services.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
