4c5eed952593975012d3e67ec74d6ee65ba1e0aa
FreddySSF <fernandoaguilarssf@gmail.com>
2023-04-27 10:57:52 -0700

    Task Service Model and Controller Updates

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TeamJacobGroupTaskManagerAppAPI.Models.DTO;
+using TeamJacobGroupTaskManagerAppAPI.Services;
 
 namespace TeamJacobGroupTaskManagerAppAPI.Controllers
 {
-    public class TaskController
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
     {
-        
+        private readonly TaskService _data;
+        public TaskController(TaskService dataFromService)
+        {
+            _data = dataFromService;
+        }
+
+        [HttpPost]
+        [Route("AddTaskItem")]
+        public bool AddTaskItem(TaskItemModel newTaskItem)
+        {
+            return _data.AddTaskItem(newTaskItem);
+        }
+
+        [HttpGet]
+        [Route("GetAllTaskItems")]
+        public IEnumerable<TaskItemModel> GetAllTaskItems()
+        {
+            return _data.GetAllTaskItems();
+        }
+
+        [HttpGet]
+        [Route("GetTasksByAsignee/{AssignedTo}")]
+        public IEnumerable<TaskItemModel> GetTasksByAsignee(string AssignedTo)
+        {
+            return _data.GetTasksByAsignee(AssignedTo);
+        }
     }
 }
--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -2,11 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TeamJacobGroupTaskManagerAppAPI.Models.DTO;
+using TeamJacobGroupTaskManagerAppAPI.Services.Context;
 
 namespace TeamJacobGroupTaskManagerAppAPI.Services
 {
     public class TaskService
     {
-        
+        private readonly DataContext _context;
+        public TaskService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddTaskItem(TaskItemModel newTaskItem)
+        {
+            _context.Add(newTaskItem);
+            return _context.SaveChanges() != 0;
+        }
+
+         public IEnumerable<TaskItemModel> GetAllTaskItems()
+        {
+            return _context.TaskInfo;
+        }
+
+          public IEnumerable<TaskItemModel> GetTasksByAsignee(string AssignedTo)
+        {
+            return _context.TaskInfo.Where(item => item.AssignedTo == AssignedTo);
+        }
     }
 }
